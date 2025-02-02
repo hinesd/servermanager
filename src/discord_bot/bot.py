a2d009d4bb03a44c922f97f5ed839eb37395a358
Dillon Hines <hinesdillon01@gmail.com>
2025-02-02 14:41:27 -0500

    updated pathing issues

--- src/discord_bot/bot.py
+++ src/discord_bot/bot.py
@@ -1,7 +1,7 @@
 import discord
 import httpx
 from discord.ext import commands
-from servermanager.config import DISCORD_TOKEN, SERVER_DOMAIN
+from config import DISCORD_TOKEN, SERVER_DOMAIN
 
 intents = discord.Intents.default()
 intents.message_content = True
