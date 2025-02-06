a86be063b3ddb59c75b7e584995aaa7081b01d5f
Dillon Hines <dillonhines@mac.tail650234.ts.net>
2025-02-05 23:59:02 -0500

    updated process_validation to ensure server connection

--- src/server_controller/api/core/minecraft.py
+++ src/server_controller/api/core/minecraft.py
@@ -1,7 +1,6 @@
 import os
-from mcrcon import MCRcon
 from mcstatus import JavaServer
-import json
+import datetime
 from asyncio import create_subprocess_shell, wait_for, to_thread, sleep
 from asyncio.subprocess import PIPE
 from pathlib import Path
@@ -13,11 +12,18 @@ class MinecraftServer:
 
     def __init__(self):
         self.process = None
+        self.server_connection = None
+        self.created_at = datetime.datetime.now()
         self.base_path = next(p for p in Path(__file__).resolve().parents if p.name == 'server_controller')
         self.server_path = f'{self.base_path}/server' if ENV_NAME == 'prod' else f'{self.base_path}/testserver/'
         self.start_script = f'{self.server_path}/start_script.sh'
-        #TODO add logic to dynamically override server.properties using attributes defined in config.py
-        self.server_properties = dict([x.split('=') for x in open(self.server_path + 'server.properties').read().strip().split('\n')[2:]])
+        self.server_properties = self._get_server_properties()
+        self.server_port = int(self.server_properties['server-port'])
+
+
+    def _get_server_properties(self):
+        #TODO add logic to dynamically override server.properties using attributes defined in config.py before load
+        return dict([x.split('=') for x in open(self.server_path + 'server.properties').read().strip().split('\n')[2:]])
 
 
     async def kill_process(self):
@@ -28,6 +34,7 @@ class MinecraftServer:
             pass
         finally:
             self.process = None
+            self.server_connection = None
 
 
     async def graceful_shutdown(self):
@@ -39,16 +46,28 @@ class MinecraftServer:
             await self.kill_process()
         finally:
             self.process = None
+            self.server_connection = None
 
 
-    async def process_validation(self):
+    async def process_validation(self, create_process=None):
         if not self.process:
             raise ProcessDoesNotExist()
         if self.process.returncode is not None:
+            # Is the process running
             stdout, stderr = await self.process.communicate()
             error_message = f"Process validation failed:\nSTDOUT: {stdout.decode()}\nSTDERR: {stderr.decode()}"
             await self.kill_process()
-            raise ProcessCreationFailed(error_message)
+            raise ProcessValidationFailed(error_message)
+        if create_process and not self.server_connection:
+            self.server_connection = JavaServer(SERVER_DNS, self.server_port, timeout=30)
+        max_retries = 10
+        while max_retries:
+            try:
+                await self.server_connection.async_ping()
+                return
+            except ConnectionRefusedError:
+                await sleep(10)
+                max_retries -= 1
 
 
     async def validate_or_create_process(self, create_process=None):
@@ -58,35 +77,26 @@ class MinecraftServer:
             await to_thread(lambda: os.chmod(self.start_script, 0o755))
             self.process = await create_subprocess_shell(self.start_script,stdin=PIPE,stdout=PIPE,stderr=PIPE,cwd=self.server_path)
             await sleep(1)
-        await self.process_validation()
-
-
-    async def get_server_status(self):
-        # todo this test is temporary, need to be cleaned up to make a bit more sense
-        max_attempts = 5
-        attempt = 0
-        while attempt < max_attempts:
-            try:
-                server = JavaServer(SERVER_DNS, int(self.server_properties.get('query.port', 25565)))
-                return json.dumps(server.status().raw)
-            except ConnectionRefusedError:
-                await sleep(5)
-        await self.graceful_shutdown()
-        raise ConnectionRefusedError
+        await self.process_validation(create_process)
 
 
     async def start(self, create_process=None):
-        #todo add logic to make sure port is not taken already to accound for this error
-        """Failed to start the minecraft server ./world/session.lock: already locked (possibly by other Minecraft instance?)"""
         if not os.path.exists(self.start_script):
             raise FileNotFoundError(self.start_script)
-        await self.validate_or_create_process(create_process=create_process)
-        #todo make this server connection test make more sense
-        server_status = await self.get_server_status()
-        return 'Server Successfully Started\n' + server_status
+        try:
+            await self.validate_or_create_process(create_process=create_process)
+        except ProcessValidationFailed:
+            raise ProcessCreationFailed()
+        return f'Server successfully started'
 
 
     async def stop(self):
-        await self.validate_or_create_process()
+        await self.process_validation()
         await self.graceful_shutdown()
         return "Successfully Stopped Server"
+
+
+    async def get_status(self, query=None):
+        await self.process_validation()
+        results = (await self.server_connection.async_status()).__dict__
+        return results
